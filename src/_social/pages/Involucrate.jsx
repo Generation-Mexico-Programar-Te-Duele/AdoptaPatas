8454ae4eeb3c7d4a0135d7b6e10da71909824a75
Janett Venegas <jannv25@gmail.com>
2023-12-31 14:40:09 -0600

    Involucrate

--- src/_social/pages/Involucrate.jsx
+++ src/_social/pages/Involucrate.jsx
@@ -13,8 +13,19 @@ function Involucrate(){
             <div className="flex justify-between items-start">
                 <div className="flex-grow text-justify">
                     <p className="involucrateParagh mb-6 text-2xl text-black font-bold">     
-                   !Hola¡  ¿Estás interesado en adoptar?
-                    En AdoptaPatas tanto usuarios como organizaciones tienen la posibilidad de adoptar, en cualquiera que sea el caso (organización o usuario) deben registrarse. 
+                   ¡Hola!  ¿Te has planteado la posibilidad de darle un hogar a un amigo peludo?
+                   </p>
+             
+                    <p className="involucrateParagh mb-6 text-2xl text-black font-bold">
+                   Los perros y los gatos son amigos leales que están contigo en los buenos y malos momentos. Además de su apoyo incondicional, tener una mascota puede tener impactos positivos en tu salud mental y física. Ya sea paseando un perro, jugando con un gato o simplemente tener una compañía afectuosa puede mejorar tu bienestar. 
+                   </p>
+             
+                    <p className="involucrateParagh mb-6 text-2xl text-black font-bold">
+                   Cuidar de una mascota va más allá de la simple compañia. Implica asumir responsabilidades, desarrollar empatía y tener disciplina. Es una experiencia enriquecedora que no solo beneficia al animal, sino también al adoptante.
+                   </p>
+             
+                    <p className="involucrateParagh mb-6 text-2xl text-black font-bold">
+                    En AdoptaPatas, ofrecemos la oportunidad tanto a usuarios individuales como a organizaciones de encontrar a su compañero. Independientemente de sí eres un individuo buscando un amigo o una organización interesada en darle hogar a una mascota, el primer paso es registrarse. ¡Únete a nuestra comunidad y haz una diferencia en la vida de estos seres!. 
                     </p>
              
                     <p className="involucrateParagh mb-6 text-2xl text-black font-bold">
@@ -30,24 +41,23 @@ function Involucrate(){
                  <img src="src\assets\img\contact\Carousel\OIG (8).jpeg" className="mx-auto max-w-[70%] rounded-[100%]" alt="mascota" />
         
             </div> */}
-                    <div className="w-[90%] mx-auto flex-basis-50">
+                    <div className=" w-[200%] mx-auto flex-basis-40">
                         <img
                             src="src/assets/img/contact/Carousel/OIG (8).jpeg"
-                            className="mx-auto max-w-[100%] rounded-[100%]" alt="mascota"
+                            className="mx-auto max-w-[90%] rounded-[100%]" alt="mascota"
                         />
                         </div>
                     </div>
-                        <link
+                        {/* <link
                          href="/sign-up"
-                        className="block text-center text-lg my-4 text-buttonColor"
-                        /> Regístrate
+                        className="block text-center text-lg my-5 text-buttonColor"
+                        /> Regístrate */}
 
                         {/* Link a Regístrate */}
                     
                     <button
                         id="buttonContact"
-
-                        className="cursor-pointer mx-auto text-center py-1 px-2 mb-1 text-[1.3rem] flex justify-center align-middle w-full"
+                        class="cursor-pointer mx-auto text-center py-1 px-2 mb-1 text-[2rem] flex justify-center align-middle w-full"
                     >
                         Regístrate
                     </button>
