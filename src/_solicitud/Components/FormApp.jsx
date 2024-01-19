db28392f6e635202593a2df1b2e695c69fbc2f6f
IzcoGLeon <Izco.g.leon@outlook.com>
2024-01-19 15:12:50 -0600

    file

--- src/_solicitud/Components/FormApp.jsx
+++ src/_solicitud/Components/FormApp.jsx
@@ -1,21 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ButtonApp from '../../_contact/components/ButtonApp';
 import { FormProvider, useForm } from 'react-hook-form';
 import { BsFillCheckSquareFill } from 'react-icons/bs'
 import { question } from '../utils/inputValidations'
 import Input from './InputApp';
 import CheckApp from './CheckApp';
+import { useNavigate } from 'react-router-dom';
+
 
 
 const FormApp = () => {
 
+  const navigate = useNavigate();
+
   const methods = useForm();
   const [success, setSuccess] = useState(false);
   const onSubmit = methods.handleSubmit(data => {
-    console.log(data);
-    setSuccess(true);
+    const values = methods.getValues()
+    console.log(values)
+    console.log(question);
     methods.reset();
-  });
+    alert("Gracias! Tu información ha sido enviada");
+    setTimeout(() => {
+      navigate('/');
+    }, 1000);
+  })
+
 
   return (
     <>
@@ -34,7 +44,7 @@ const FormApp = () => {
 
           {success && (
             <p className="flex items-center gap-1 mb-5 font-semibold text-green-500">
-              <BsFillCheckSquareFill />Tu información ha sido enviada de manera exitosa.
+              <BsFillCheckSquareFill />Tu mensaje ha sido enviado de manera exitosa.
             </p>
           )}
 
