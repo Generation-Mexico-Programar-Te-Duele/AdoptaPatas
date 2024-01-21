f863cf79d60a17a93be11545336bb7be5267a186
Daniel Iñiguez <danielig9@outlook.com>
2024-01-21 00:46:03 -0600

    **/*.JPG

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   build: {
-    assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'],
+    assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg', "**/*.JPG"],
   },
 });
 
